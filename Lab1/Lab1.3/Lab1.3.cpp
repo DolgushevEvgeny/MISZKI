e3943ec1973dae696a3671500e1f200ce9695ca9
Dolgushev Evgeny <baraxlush1995@gmail.com>
2016-11-04 15:26:21 +0300

    Доработки
    Доработана 3 часть

--- Lab1/Lab1.3/Lab1.3.cpp
+++ Lab1/Lab1.3/Lab1.3.cpp
@@ -64,11 +64,15 @@ bool attack(const vector<char> &encrypted, char &key) {
 		return true;
 	}
 	else {
+		if (keys.size() > 0) {
+			key = *(--keys.end());
+			return true;
+		}
 		return false;
 	}
 }
 
-ifstream ReadableFile(string fileName) {
+ifstream ReadableFile(const string &fileName) {
 	ifstream inputFile;
 	inputFile.open(fileName, ifstream::in | ifstream::binary);
 
@@ -80,6 +84,20 @@ ifstream ReadableFile(string fileName) {
 	return inputFile;
 }
 
+vector<char> ReadFromFile(ifstream &file) {
+	vector<char> buffer;
+	while (!file.eof()) {
+		char t;
+		file.get(t);
+		if (file.eof()) {
+			break;
+		}
+		buffer.push_back(t);
+	}
+
+	return buffer;
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 2) {
@@ -89,24 +107,55 @@ int main(int argc, char* argv[])
 
 	auto inputFile = ReadableFile(argv[1]);
 
-	vector<char> buffer;
-	while (!inputFile.eof()) {
-		char t;
-		inputFile.get(t);
-		if (inputFile.eof()) {
-			break;
+	vector<char> buffer = ReadFromFile(inputFile);
+
+	bool flag = true;
+	int shift = 1;
+	int steps = 1;
+	vector<char> resultKey;
+	while (flag) {
+		resultKey.clear();
+		for (int i = 0; i < steps; ++i) {
+			vector<char> temp;
+			for (int j = i; j < buffer.size(); j += shift) {
+				cout << buffer[j];
+				temp.push_back(buffer[j]);
+			}
+			char key;
+			if (attack(temp, key)) {
+				cout << key;
+				resultKey.push_back(key);
+				if (i == steps - 1) {
+					flag = false;
+					break;
+				}
+			}
+			else {
+				cout << "damn";
+			}
 		}
-		buffer.push_back(t);
+		++steps;
+		++shift;
 	}
 
-	char key;
+	if (resultKey.size() == steps - 1) {
+		cout << "ready";
+		for (int i = 0; i < resultKey.size(); ++i) {
+			cout << resultKey[i];
+		}
+		
+	} else {
+		cout << "DAMN";
+	}
+
+	/*char key;
 	if (attack(buffer, key)) {
 		cout << key;
 	}
 	else {
 		cout << "damn";
 	}
-	cout << "ready";
+	cout << "ready";*/
 
 	return 0;
 }
